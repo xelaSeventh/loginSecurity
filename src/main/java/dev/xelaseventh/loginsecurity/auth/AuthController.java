644d41144eb820506705b2d80e495a1ff3097036
xelaSeventh <afx.xela@gmail.com>
2024-02-26 19:59:03 -0600

    Project Strucure & DB Connection

---
+++ src/main/java/dev/xelaseventh/loginsecurity/auth/AuthController.java
@@ -0,0 +1,13 @@
+package dev.xelaseventh.loginsecurity.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+}
