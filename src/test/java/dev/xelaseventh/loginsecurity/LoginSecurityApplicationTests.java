bc736eae9a8fb639cb7236592bd93b5f1706dc08
xelaSeventh <afx.xela@gmail.com>
2024-02-26 19:48:17 -0600

    First Commit

---
+++ src/test/java/dev/xelaseventh/loginsecurity/LoginSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.xelaseventh.loginsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
